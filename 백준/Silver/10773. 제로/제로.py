484da4c0d484f03c2250f1a1fbfb9c8a3650618f
Seo_Gyeongwon <108510272+ae-GYEONGWON@users.noreply.github.com>
2023-04-15 20:45:45 +0900

    [Silver IV] Title: 제로, Time: 88 ms, Memory: 32036 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,10 +1,15 @@
+import sys
+input = sys.stdin.readline
 k = int(input())
 
-stack = []
+stack = [0]*100000
+ptr = 0
 for i in range(k):
     num = int(input())
     if num == 0:
-        stack.pop()
+        stack[ptr-1] = 0
+        ptr -= 1
     else:
-        stack.append(num)
+        stack[ptr] = num
+        ptr += 1
 print(sum(stack))
