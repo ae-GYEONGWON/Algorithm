b8e48960b68fe8f51bafafa896cd72860a816a2d
Seo_Gyeongwon <108510272+ae-GYEONGWON@users.noreply.github.com>
2023-04-26 01:12:29 +0900

    [Gold V] Title: 동전 2, Time: 460 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,28 +1,16 @@
 import sys
-
+import math
 input = sys.stdin.readline
-
-n, k = list(map(int, input().split()))
+n, k = map(int, input().split())
 coins = [int(input()) for _ in range(n)]
-
-dp = [0] * (k+1)
+dp = [0]+[math.inf]*k
 
 for coin in coins:
-    for i in range(1, len(dp)):
-        if i-coin > 0:
-            if dp[i] == 0 or dp[i] == -1:
-                if dp[i-coin] == -1:
-                    dp[i] = -1
-                else:
-                    dp[i] = dp[i-coin] + 1
-            else:
-                if dp[i-coin] == -1:
-                    continue
-                else:
-                    dp[i] = min(dp[i], dp[i-coin] + 1)
-        # i와 coin이 같으므로 동전 한개로 i원을 만들 수 있음.
-        elif i-coin == 0:
-            dp[i] = 1
-        elif i-coin < 0 and dp[i] == 0:
-            dp[i] = -1
-print(dp[k])
+    for i in range(1, k+1):
+        if i-coin >= 0:
+            dp[i] = min(dp[i-coin]+1, dp[i])
+
+if dp[-1] == math.inf:
+    print(-1)
+else:
+    print(dp[-1])
