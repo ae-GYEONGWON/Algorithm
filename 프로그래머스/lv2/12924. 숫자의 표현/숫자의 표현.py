ce35e78a4c8af989cabc05d2196e39d292fc117e
Seo_Gyeongwon <108510272+ae-GYEONGWON@users.noreply.github.com>
2023-09-10 20:57:17 +0900

    [level 2] Title: 숫자의 표현, Time: 5.65 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,17 @@
+def solution(n):
+    answer = 0
+    
+    for i in range(1, n+1):
+        temp = 0
+        for j in range(i, n+1):
+            temp += j
+            if temp == n:
+                
+                answer += 1
+                break
+            if temp > n:
+                break
+        
+    1234
+    10
+    return answer
