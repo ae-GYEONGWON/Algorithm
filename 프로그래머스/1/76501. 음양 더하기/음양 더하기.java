e16c32f7d0229137c2918bee6ce941cf09715c61
Seo_Gyeongwon <108510272+ae-GYEONGWON@users.noreply.github.com>
2023-10-29 21:13:29 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for (int i = 0; i<absolutes.length; i++) {
+            int temp = absolutes[i];
+            if (!signs[i]) {
+                temp *= -1;
+            }
+            answer += temp;
+        }
+        
+        return answer;
+    }
+}
