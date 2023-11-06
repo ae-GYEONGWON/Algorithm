c53ddb078bfbec1aabefdf3c449ecd229b7f7075
Seo_Gyeongwon <108510272+ae-GYEONGWON@users.noreply.github.com>
2023-11-06 23:58:01 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = ''
+    center = len(s)//2
+    
+    if (len(s)%2 == 0):
+        return s[center-1:center+1]
+    else:
+        return s[center]
+    
+    return answer
