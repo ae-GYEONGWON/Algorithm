a6aff3d63f13f070b71641b886b164809a4fc83d
Seo_Gyeongwon <108510272+ae-GYEONGWON@users.noreply.github.com>
2024-06-21 17:00:25 +0900

    [level 2] Title: 2 x n 타일링, Time: 4.74 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -1,28 +1,25 @@
-# def solution(n):    
-#     '''
-#     a1 = 1
-#     a2 = 2
-#     a3 = 3
-#     a4 = 5
-#     a5 = 8
-#     a6 = 13
-#     a8 = 
-#     '''
+def solution(n):    
+    '''
+    a1 = 1
+    a2 = 2
+    a3 = 3
+    a4 = 5
+    a5 = 8
+    a6 = 13
+    a8 = 
+    '''
     
-#     dp = [1, 1, 2, 3, 5]
+    a = 1; b = 1
     
-#     if n < 5:
-#         return dp[n]
+    for _ in range(n):
+        a, b = b, (a+b)%1000000007
     
-#     for i in range(5, n+1):
-#         dp[i] = (dp[i-1] + dp[i-2])%1000000007
-    
-#     return dp[n]
+    return a
 
-def solution(n):
-    dp = [0 for i in range(n)]
-    dp[0], dp[1] = 1, 2
-    for i in range(2, n):
-        dp[i] = (dp[i-1] + dp[i-2]) % 1000000007
+# def solution(n):
+#     dp = [0 for i in range(n)]
+#     dp[0], dp[1] = 1, 2
+#     for i in range(2, n):
+#         dp[i] = (dp[i-1] + dp[i-2]) % 1000000007
 
-    return dp[n-1]
+#     return dp[n-1]
