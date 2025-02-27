9225fbd211a494d1980b14e707d4ab22299398d7
Seo_Gyeongwon <108510272+ae-GYEONGWON@users.noreply.github.com>
2025-02-27 22:54:39 +0900

    [level 0] Title: 약수 구하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -3,7 +3,7 @@ def solution(n):
     
     for e in range(1, int(n**0.5)+1):
         if n % e == 0:
-            answer.extend([e, n//e])
-    answer = list(set(answer))
+            answer.extend([e, n//e]) if e != n//e else answer.append(e)
+
     answer.sort()
     return answer
